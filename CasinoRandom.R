c02b5b401c16b9ed12e2be43622bd11938ffbf4d
Vegeta <cathorian.vegeta@gmail.com>
2021-07-17 13:24:22 +0100

    Adding random winner picker

---
+++ CasinoRandom.R
@@ -0,0 +1,52 @@
+# v0.0.2
+
+# -----
+
+# POT with 10 tickets.
+
+
+draw = TRUE  # Generates winning numbers if set true
+
+
+message(Sys.time())                                                     # Current datetime
+
+number.of.tickets = 10                                                  # Number of tickets
+house.share.to.pot = 5
+ticket.price = 1                                                        # Ticket price
+size.of.pot = (ticket.price * number.of.tickets) + house.share.to.pot   # Size of the pot
+
+winner.share = c(0.5, 0.3, 0.1)                                         # Winning proportion
+winner.title = c(
+        "**** First winner  **** :",
+        "**** Second winner **** :",
+        "**** Third winner  **** :"
+)
+winners = sample(1:size.of.pot, size = 3, replace = FALSE)
+
+
+
+if (!draw) {
+        winners = rep("--", 3)
+        message("\t--- No draw made --- ")
+}
+
+print("------------------------------------------------\n")
+for (index in 1:length(winners)) {
+        winner.text = paste(
+                c(
+                        winner.title[index],
+                        "(",
+                        winners[index],
+                        ") ||",
+                        size.of.pot * winner.share[index],
+                        " == (",
+                        size.of.pot,
+                        "CTHOR) X ",
+                        winner.share[index]
+                ),
+                col = ""
+        )
+        message(winner.text)
+        
+}
+
